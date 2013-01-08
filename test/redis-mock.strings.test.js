803ec81712466e2e7fcb1463dbfe5403bb07787a
Adam Howard <adz@skatty.me>
2013-01-08 23:16:05 +0000

    Add SETEX command

--- test/redis-mock.strings.test.js
+++ test/redis-mock.strings.test.js
@@ -6,8 +6,8 @@ describe("get", function () {
 
     it("should return the value of an existing key", function(done) {
         var r = redismock.createClient("", "", "");
-        r.set("foo", "bar", function (err, result) {
-            r.get("foo", function (err, result) {
+        r.set("foo1", "bar", function (err, result) {
+            r.get("foo1", function (err, result) {
                 result.should.equal("bar");
                 r.end();
                 done();
@@ -21,6 +21,38 @@ describe("get", function () {
             should.not.exist(result);
             r.end();
             done();
+        });
+    });
+
+});
+
+describe("setex", function () {
+
+    it("should set the key", function (done) {
+      var r = redismock.createClient("", "", "");
+
+      r.setex("foo2", 1000, "bar", function (err, result) {
+          r.get("foo2", function (err, result) {
+              result.should.equal("bar");
+              r.end();
+              done();
+          });
+      });
+    });
+
+    it("should make key disappear after the set time", function (done) {
+        var r = redismock.createClient("", "", "");
+
+        r.setex("foo3", 1, "bar", function (err, result) {
+            result.should.equal('OK');
+
+            setTimeout(function () {
+                r.exists("foo3", function (err, result) {
+                    result.should.equal(0);
+                    r.end();
+                    done();
+                });
+            }, 2000);
         });
     });
 
